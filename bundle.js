47b2060c928bca03a4f58abe91e0bfc763525df5
WH <waqashsn@gmail.com>
2018-02-07 21:22:49 +0500

    generated bundle.js by running webpack manually from cli

---
+++ bundle.js
@@ -0,0 +1,101 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+//function to see if a word exists in any row of a two dim array
+function exists(arr, word){
+    return arr.some(row => row.includes(word));
+
+}
+
+
+//function to convert the target string into a two dim array with word count
+function toTwoDimArray(text){
+    let textArray = text.split(" "); // convert target string into a single dim array
+    console.log(textArray.length); 
+    var twoDimArray = []; //declare a two dim array
+    for(i = 0; i < textArray.length; i++){
+        if(!exists(twoDimArray, textArray[i])){
+            twoDimArray[i] = [textArray[i]]
+            twoDimArray[i][1] = 1;
+        }
+        else{
+            for(x = 0; x < twoDimArray.length; x++){
+                if(twoDimArray[x][0] == textArray[i])
+                {
+                    twoDimArray[x][1]++;
+                }
+            }
+        }  
+    
+    }
+    return twoDimArray;
+}
+
+/***/ })
+/******/ ]);
